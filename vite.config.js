d17ffc0e34082b655cc3611a2a458363160d19f9
Isaias Santana dos Santos <isaiassantanadossantos@hotmail.com>
2023-06-23 22:32:07 -0300

    deploy configutation

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/portfolio",
+  base: "/domain-test-in-project",
   server: {
     // Configuração para permitir o download de arquivos
     serveStatic: {
